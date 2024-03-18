834c3a3101fc74fd5e738c639a3fb3320cec44e8
bugogat <162794631+bugogat@users.noreply.github.com>
2024-03-18 17:47:56 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+my_string = input()
+print(input())
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(''.join(my_string.split()))
+print(my_string[0])
+print(my_string[-1])
